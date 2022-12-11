6a4b3e721ab7a1f8f0f8bf1427b8280a376f26c9
hoanglongne <73406617+hoanglongne@users.noreply.github.com>
2022-12-11 22:59:20 +0700

    .

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'display': ['Bungee'],
+			'sans': ['Inria Sans'],
+			'tech': ['Share Tech']
+		},
+		extend: {
+			colors: {
+			'main-blue': '#1F2451',
+			'gray-blue': 'rgba(31, 36, 81, 0.45)',
+			'red': '#D2636E',
+			'light-red': '#EBAEBF',
+			'tech': 'rgba(31, 36, 81, 0.5);'
+			},
+			dropShadow: {
+				'pink': '3px 0 0 rgba(231, 103, 103, 0.25)',
+			},
+		},
+		letterSpacing: {
+			'widest' : '.375em'
+		},
+	plugins: [],
+	}
+}
